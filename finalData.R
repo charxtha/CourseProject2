0cf22bbccaf994bcf7fcf6337be19f70286b369d
charxtha <126442953+charxtha@users.noreply.github.com>
2023-05-06 17:30:58 +0530

    Add files via upload

---
+++ finalData.R
@@ -0,0 +1,23 @@
+> FinalData
+# A tibble: 180 × 88
+# Groups:   subject [30]
+subject activity       TimeBodyAcceleromete…¹ TimeBodyAcceleromete…²
+<int> <chr>                           <dbl>                  <dbl>
+1       1 LAYING                          0.222               -0.0405 
+2       1 SITTING                         0.261               -0.00131
+3       1 STANDING                        0.279               -0.0161 
+4       1 WALKING                         0.277               -0.0174 
+5       1 WALKING_DOWNS…                  0.289               -0.00992
+6       1 WALKING_UPSTA…                  0.255               -0.0240 
+7       2 LAYING                          0.281               -0.0182 
+8       2 SITTING                         0.277               -0.0157 
+9       2 STANDING                        0.278               -0.0184 
+10       2 WALKING                         0.276               -0.0186 
+# ℹ 170 more rows
+# ℹ abbreviated names: ¹​TimeBodyAccelerometer.mean...X,
+#   ²​TimeBodyAccelerometer.mean...Y
+# ℹ 84 more variables: TimeBodyAccelerometer.mean...Z <dbl>,
+#   tGravityAccelerometer.mean...X <dbl>,
+#   tGravityAccelerometer.mean...Y <dbl>,
+#   tGravityAccelerometer.mean...Z <dbl>, …
+# ℹ Use `print(n = ...)` to see more rows
